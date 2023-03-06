ae3dc20463bfc9d601330e76b45fa2729972e9df
Aardhynlavneder <laveat1@student.op.ac.nz>
2023-03-06 17:08:32 +1300

    build(server): connection string not required

--- server/environment/index.ts
+++ server/environment/index.ts
@@ -5,7 +5,7 @@ config({ path: "./.env" });
 //        store keys in array, and generate and export an object and assertion functions
 
 export const EXPRESS_PORT = process.env.EXPRESS_PORT;
-export const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+//export const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
 
 export const DB_USER = process.env.DB_USER;
 export const DB_PORT = process.env.DB_PORT;
@@ -15,8 +15,8 @@ export const DB_PASSWORD = process.env.DB_PASSWORD;
 
 export function AssetEnvironment() {
   if (!EXPRESS_PORT) throw new Error("`EXPRESS_PORT` is not defined");
-  if (!DB_CONNECTION_STRING)
-    throw new Error("`DB_CONNECTION_STRING` is not defined");
+  //if (!DB_CONNECTION_STRING)
+    //throw new Error("`DB_CONNECTION_STRING` is not defined");
   if (!DB_NAME) throw new Error("`DB_NAME` is not defined");
   if (!DB_HOST) throw new Error("`DB_HOST` is not defined");
   if (!DB_PASSWORD) throw new Error("`DB_PASSWORD` is not defined");

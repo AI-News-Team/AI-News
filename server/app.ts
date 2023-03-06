f5383e495292d50ef577431e698ed00e9e31dc2d
Aardhynlavneder <laveat1@student.op.ac.nz>
2023-03-06 17:40:47 +1300

    chore(quality): pretty code

--- server/app.ts
+++ server/app.ts
@@ -1,32 +1,32 @@
-import express from "express";
-import { EXIT_ERROR, EXIT_SUCCESS } from "./constant/code";
-import { connectClient, disconnectClient } from "./database";
-import { EXPRESS_PORT } from "./environment";
-import route from "./route";
+import express from 'express';
+import { EXIT_ERROR, EXIT_SUCCESS } from './constant/code';
+import { connectClient, disconnectClient } from './database';
+import { EXPRESS_PORT } from './environment';
+import route from './route';
 
 connectClient(); // Connect to the database
 
 const instance = express();
 instance.use(express.json());
 instance.use(express.urlencoded({ extended: true }));
-instance.use("/", route);
+instance.use('/', route);
 
 const server = instance.listen(EXPRESS_PORT, () => {
-  console.log(`${EXPRESS_PORT} 🚀 Online`);
+	console.log(`${EXPRESS_PORT} 🚀 Online`);
 });
 
-process.on("uncaughtException", uncaughtException);
+process.on('uncaughtException', uncaughtException);
 function uncaughtException(err: Error) {
-  console.error(err);
-  process.exit(EXIT_ERROR);
+	console.error(err);
+	process.exit(EXIT_ERROR);
 }
 
-process.on("SIGINT", terminate); // on Ctrl+C
-process.on("SIGTERM", terminate); // on `kill`
+process.on('SIGINT', terminate); // on Ctrl+C
+process.on('SIGTERM', terminate); // on `kill`
 function terminate() {
-  server.close(() => {
-    disconnectClient();
-    console.log(`${EXPRESS_PORT} Offline!`);
-    process.exit(EXIT_SUCCESS);
-  });
+	server.close(() => {
+		disconnectClient();
+		console.log(`${EXPRESS_PORT} Offline!`);
+		process.exit(EXIT_SUCCESS);
+	});
 }
--- server/environment/index.ts
+++ server/environment/index.ts
@@ -1,18 +1,18 @@
-import { config } from "dotenv";
+import { config } from 'dotenv';
 
-config({ path: "./.env" });
+config({ path: './.env' });
 
 type Primitive = string | number | boolean | symbol | bigint | undefined | null;
 function assertVariable<T extends Primitive = string>(name: string) {
-  const variable = process.env[name];
-  if (!variable) throw new Error(`\`${name}\` is not defined`);
-  return variable as T;
+	const variable = process.env[name];
+	if (!variable) throw new Error(`\`${name}\` is not defined`);
+	return variable as T;
 }
 
-export const EXPRESS_PORT = assertVariable<number>("EXPRESS_PORT");
+export const EXPRESS_PORT = assertVariable<number>('EXPRESS_PORT');
 // export const DB_CONNECTION_STRING = assertVariable("DB_CONNECTION_STRING");
-export const DB_USER = assertVariable("DB_USER");
-export const DB_PORT = assertVariable<number>("DB_PORT");
-export const DB_NAME = assertVariable("DB_NAME");
-export const DB_HOST = assertVariable("DB_HOST");
-export const DB_PASSWORD = assertVariable("DB_PASSWORD");
+export const DB_USER = assertVariable('DB_USER');
+export const DB_PORT = assertVariable<number>('DB_PORT');
+export const DB_NAME = assertVariable('DB_NAME');
+export const DB_HOST = assertVariable('DB_HOST');
+export const DB_PASSWORD = assertVariable('DB_PASSWORD');

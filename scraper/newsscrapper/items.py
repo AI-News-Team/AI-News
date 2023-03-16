aa0203067b31a7db6903656098fe0d73ec88b1f0
Artem Kechemaev (1000099996) <KECHA1@student.op.ac.nz>
2023-03-16 14:37:04 +1300

    chore(scrapy): create a function to send data to db

--- scraper/newsscrapper/items.py
+++ scraper/newsscrapper/items.py
@@ -5,7 +5,7 @@ from scrapy.item import Item, Field
 class Article(scrapy.Item):
     name = scrapy.Field()
     author = scrapy.Field()
-    date = scrapy.Field()
+    publication_date = scrapy.Field()
     category = scrapy.Field()
     body = scrapy.Field()
     source_url = scrapy.Field()

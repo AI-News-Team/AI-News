d776f424edc701e8d2515806c91d2ead731c2dab
GSeal <sealgp1@student.op.ac.nz>
2023-03-17 16:21:42 +1300

    style: style article page

--- client/src/components/page-components/StoryCard.tsx
+++ client/src/components/page-components/StoryCard.tsx
@@ -12,7 +12,7 @@ const StoryCard = ({ image, title, body, id }: Props) => {
     // console.log(id)
 
     return (
-        <div className="w-[31%] mt-5 h-80">
+        <div className="w-[31%] my-8 h-80">
             <Link to={`/article/${id}`} ><img src={image} alt="" className='object-cover h-72 w-full' /></Link>
             <div className="bg-white p-2 border-l-2 border-r-2 border-b-2 h-14">
                 <h3 className='font-bold'>{title}</h3>

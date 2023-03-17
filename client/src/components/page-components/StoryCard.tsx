a4aad1e2fe3f4335d905e514ebdb0ac0e764608a
GSeal <sealgp1@student.op.ac.nz>
2023-03-17 16:33:14 +1300

    styling

--- client/src/components/page-components/StoryCard.tsx
+++ client/src/components/page-components/StoryCard.tsx
@@ -12,10 +12,11 @@ const StoryCard = ({ image, title, body, id }: Props) => {
     // console.log(id)
 
     return (
-        <div className="w-[31%] my-8 h-80">
+        <div className="w-[31%] pb-12">
             <Link to={`/article/${id}`} ><img src={image} alt="" className='object-cover h-72 w-full' /></Link>
-            <div className="bg-white p-2 border-l-2 border-r-2 border-b-2 h-14">
+            <div className="bg-white p-4 border-l-2 border-r-2 border-b-2 h-24">
                 <h3 className='font-bold'>{title}</h3>
+                <p className="truncate text-gray-500">{body}</p>
             </div>
         </div>
     )

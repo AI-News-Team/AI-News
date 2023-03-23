8b5ff912b970aaf14a973f2105deb0038ba7af4b
GSeal <sealgp1@student.op.ac.nz>
2023-03-23 13:28:44 +1300

    change link tag in compact story card

--- client/src/components/page-components/CompactStoryCard.tsx
+++ client/src/components/page-components/CompactStoryCard.tsx
@@ -12,7 +12,7 @@ const CompactStoryCard = ({ image, title, id }: Props) => {
         <div className="w-full">
             <a href={`/article/${id}`} ><img src={image} alt="" className='object-cover h-40 w-full' /></a>
             <div className="bg-white pb-3 h-26 mb-5">
-                <a href={`/article/${id}`} ><h3 className='font-bold leading-5'>{title}</h3></a>
+                <Link to={`/article/${id}`} ><h3 className='font-bold leading-5'>{title}</h3></Link>
             </div>
         </div>
     )

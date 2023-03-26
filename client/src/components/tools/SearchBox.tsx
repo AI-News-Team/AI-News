abf647cf7f23834fad7843637940aa72541da6a8
gpseal <sealgp1@student.op.ac.nz>
2023-03-26 21:14:18 +1300

    add footer navigation with search bar

---
+++ client/src/components/tools/SearchBox.tsx
@@ -0,0 +1,20 @@
+const SearchBox =() => {
+
+    return (
+      <>
+        <form className="bg-white flex p-2 ">
+          <input
+            className="w-full"
+            type="text"
+            placeholder="Search AI Daily...."
+            name="search"
+          />
+          <button className="ml-auto px-1" type="submit">
+            <i className="fa-solid fa-magnifying-glass" />
+          </button>
+        </form>
+      </>
+    );
+}
+
+export default SearchBox

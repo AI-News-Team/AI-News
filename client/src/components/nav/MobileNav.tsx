f264124d62d16a175afd55960a88ebbefad81f24
gpseal <sealgp1@student.op.ac.nz>
2023-04-27 20:38:10 +1200

    adjust mobile nav

--- client/src/components/nav/MobileNav.tsx
+++ client/src/components/nav/MobileNav.tsx
@@ -3,7 +3,7 @@ import SearchBox from "../tools/SearchBox";
 import HomeLogo from "../graphics/HomeLogo";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
 
 
 type Props = {
@@ -12,31 +12,37 @@ type Props = {
 
 const MobileNav = ({ topics }: Props) => {
 
-    const [menuVisibility, setMenuVisibility] = useState(false);
+    const [menuVisibile, setMenuVisibile] = useState(false);
     const show = true;
 
-    const setMenuDisplay = () => {setMenuVisibility(!menuVisibility);};
+    const setMenuDisplay = () => {setMenuVisibile(!menuVisibile);};
 
   //not sure why, but this is necessary to have the active menu item change colour
   console.log(useResolvedPath("/topic1"));
 
   return (
-    <nav className="w-full text-white h-16 bg-black fixed z-20">
-      <div className="h-full flex justify-between">
+    <nav className="w-full text-white h-12 bg-black fixed z-20">
+      <div className="h-full flex justify-between border-b border-slate-700">
         <div className="w-8 h-full"></div>
         <div className="my-auto">
-          <HomeLogo size={4} />
+          <HomeLogo size={2} />
         </div>
         <button onClick={setMenuDisplay} className="w-8 text-center">
-          <FontAwesomeIcon icon={faCaretDown} size="xl" />
+          <FontAwesomeIcon icon={faEllipsisVertical} />
         </button>
       </div>
-      <div className={menuVisibility ? menuShow : menuHidden}>
+      <div
+        className={
+          menuVisibile
+            ? "transition ease-in-out delay-150 z-10 w-full absolute bg-black pb-5 flex flex-col items-center"
+            : "hidden"
+        }
+      >
         {topics.map((topic) => (
           <div key={topic} className="flex justify-center">
             <Link
               onClick={setMenuDisplay}
-              className={`bg-black w-screen p-2 text-sm text-center border-t border-slate-700 ${
+              className={`bg-black w-screen p-2 text-sm text-center border-b border-slate-700 ${
                 window.location.pathname == `/${topic}`
                   ? "bg-slate-500"
                   : "bg-black"
@@ -47,7 +53,7 @@ const MobileNav = ({ topics }: Props) => {
             </Link>
           </div>
         ))}
-        <div className="items-center w-11/12">
+        <div className="items-center w-11/12 pt-5">
           <SearchBox />
         </div>
       </div>
@@ -55,9 +61,5 @@ const MobileNav = ({ topics }: Props) => {
   );
 };
 
-const menuHidden =
-  "transition ease-in-out z-10 w-full absolute bottom-[100%] bg-black pb-5 flex flex-col items-center";
-const menuShow =
-  "transition ease-in-out z-10 w-full absolute top-16 bg-black pb-5 flex flex-col items-center";
 const liStyle = "hover:bg-pink-800 flex items-center px-5";
 export default MobileNav;

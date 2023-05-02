69480a8ef18136f5ee11f6163e0919fa099e2ba6
Aardhynlavneder <laveat1@student.op.ac.nz>
2023-05-02 12:40:15 +1200

    feat(search): environment checks

--- search/init.local.sh
+++ search/init.local.sh
@@ -1,3 +1,10 @@
+if [ -z .env ] ; then
+  echo "Unable to initalize search engine!"
+  echo "Missing `.env` file ❌";
+  exit 1;
+fi
+source .env;
+
 pip install -r requirements.txt
 # python main.py # cmdln version
-python -m flask --app main.py run --debug -h localhost -p 6464 
+python -m flask --app main.py run --debug -h $SEARCH_ENGINE_HOST -p $SEARCH_ENGINE_PORT

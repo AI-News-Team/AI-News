f246f277e6d20ebb861c11d8e392d61b97419755
Artem Kechemaev (1000099996) <KECHA1@student.op.ac.nz>
2023-05-04 14:58:49 +1200

    fix(spider/newYorkTimes): Reduced the amount of null fields

--- scraper/newsscrapper/spiders/newYorkTimes.py
+++ scraper/newsscrapper/spiders/newYorkTimes.py
@@ -46,30 +46,38 @@ class newYorkTimesSpider(scrapy.Spider):
 
         if response.xpath('//span[@class="g-name"]'):
             item['author'] = response.xpath('//span[@class="g-name"]/text()').get()
-
-        item['author'] = response.xpath('//span[@itemprop="name"]/a/text()').get() or "New York Times"
+        else:
+            item['author'] = response.xpath('//span[@itemprop="name"]/a/text()').get() or "New York Times"
 
         if response.xpath('//div[@class="g-date"]'):
             item['publication_date'] = response.xpath('//div[@class="g-date"]/text()').get()
-        
-        item['publication_date'] = response.xpath('//time/@datetime').get()
+        elif response.xpath('//time[@class="rad-datetime"]'):
+            item['publication_date'] = response.xpath('//time[@class="rad-datetime"]/text()').get()
+        else:
+            item['publication_date'] = response.xpath('//time/@datetime').get()
 
         if response.xpath('//p[@class="g-body "]'):
             item['body'] = response.xpath('//p[@class="g-body "]/text()').getall()
-        
-        if response.xpath('//div[@data-testid="document-block-body"]/p'):
+        elif response.xpath('//div[@data-testid="document-block-body"]/p'):
             item['body'] = response.xpath('//div[@data-testid="document-block-body"]/p[position() < last()]//text()').getall()
-
-        item['body'] = response.xpath('//section[@name="articleBody"]/div/div//p/text()').getall()
+        elif response.xpath('//p[@class="paragraph"]'):
+            item['body'] = response.xpath('//p[@class="paragraph"]/text()').getall()
+        elif response.xpath('//div[@class="intro-wrapper"]/p'):
+            item['body'] = response.xpath('//div[@class="intro-wrapper"]/p//text()').getall()
+        else:
+            item['body'] = response.xpath('//section[@name="articleBody"]/div/div//p/text()').getall()
 
         item['source_url'] = response.url
 
         if response.xpath('//div[@class="g-asset_inner"]/picture/img'):
             item['cover_url'] = response.xpath('//div[@class="g-asset_inner"]/picture/img/@src').get()
-
-        if response.xpath('//header[@data-testid="title-byline"]/figure/div/div/picture[@class=""]'):
+        elif response.xpath('//header[@data-testid="title-byline"]/figure/div/div/picture[@class=""]'):
             item['cover_url'] = response.xpath('//header[@data-testid="title-byline"]/figure/div[@data-testid="image-holder"]/div/picture/img/@src').get()
-
-        item['cover_url'] = response.xpath('//picture/img/@srcset').get()
+        elif response.xpath('//div[@class="css-122y91a"]/img'):
+            item['cover_url'] = response.xpath('//div[@class="css-122y91a"]/img/@src').get()
+        elif response.xpath('//div[@class="css-122y91a"]/img'):
+            item['cover_url'] = response.xpath('//div[@class="css-122y91a"]/img/@src').get()
+        else:
+            item['cover_url'] = response.xpath('//picture/img/@srcset').get()
         
         yield item

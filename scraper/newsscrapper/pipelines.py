c2114838519d6b31b54b2e40f9e91ddfa36c144b
GSeal <sealgp1@student.op.ac.nz>
2023-05-15 16:01:32 +1200

    add rewriter to pipeline

--- scraper/newsscrapper/pipelines.py
+++ scraper/newsscrapper/pipelines.py
@@ -4,6 +4,22 @@ from itemadapter import ItemAdapter
 from dotenv import load_dotenv
 import os
 from requests.exceptions import RequestException
+import torch
+from transformers import PegasusForConditionalGeneration, PegasusTokenizer
+model_name = 'tuner007/pegasus_paraphrase'
+torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
+tokenizer = PegasusTokenizer.from_pretrained(model_name)
+model = PegasusForConditionalGeneration.from_pretrained(model_name).to(torch_device)
+
+def get_response(input_text,num_return_sequences,num_beams):
+  batch = tokenizer([input_text],truncation=True,padding='longest',max_length=60, return_tensors="pt").to(torch_device)
+  translated = model.generate(**batch,max_length=60,num_beams=num_beams, num_return_sequences=num_return_sequences, temperature=1.5)
+  tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
+  return tgt_text
+# import reWriter
+
+num_beams = 1
+num_return_sequences = 1
 
 load_dotenv()
 PORT_NUMBER = os.getenv("PORT_NUMBER")
@@ -16,11 +32,23 @@ class NewsscrapperPipeline:
     def process_item(self, item, spider):
         self.json.append(item)
         return item
+    
      
     def close_spider(self, spider):
         def serialize_item(item):
             # Return a dictionary of the item's non-underscored attributes
             return {k: v for k, v in item.items() if not k.startswith('_')}
+        
+        print(self.json[0]["body"][1])
+
+        for i in range(len(self.json[0]["body"])):
+            self.json[0]["body"][i] = get_response(self.json[0]["body"][i],num_return_sequences,num_beams)
+    
+        # for article in self.json:
+        #     for i in range(len(article['body'])):
+        #         article['body'][i] = get_response(article['body'][i],num_return_sequences,num_beams)
+
+        # print(self.json)
         
         data = json.dumps(self.json, default=serialize_item)
         articles = json.loads(data)

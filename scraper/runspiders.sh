7bdd15791f4d0a626f4efc4e70692b030bec946c
Aardhynlavneder <laveat1@student.op.ac.nz>
2023-05-30 20:33:19 +1200

    feat(cron): scraper runs periodically

--- scraper/runspiders.sh
+++ scraper/runspiders.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-cd newsscrapper/spiders
+echo 'starting scraping...' >> /var/log/cron.log 2>&1
+cd /scraper/newsscrapper/spiders
+
+# ensure environment variables are set
+# (might be run by cron)
+PATH=$PATH:/usr/local/bin
+export PATH
+
+source /scraper/container.env
+echo "Sending scraped data to $API_HOST:$API_PORT" >> /var/log/cron.log 2>&1
+
 spiders=("cnn" "newYorkTimes" "bbc" "foxNews")
 for spider in "${spiders[@]}"; do
-    echo "crawling $spider..."
-    scrapy crawl $spider
+    echo "crawling $spider..." >> /var/log/cron.log 2>&1 
+    scrapy crawl $spider >> /var/log/cron.log 2>&1
 done
-echo 'scraping complete!'
+echo 'scraping complete!' >> /var/log/cron.log 2>&1

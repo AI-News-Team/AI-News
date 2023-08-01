8436c850f0f3cb9469d43f7e9a03d1a0258934fe
Aardhynlavneder <laveat1@student.op.ac.nz>
2023-08-02 11:19:34 +1200

    fix: null logs

--- reWriter/reWriter.cron.sh
+++ reWriter/reWriter.cron.sh
@@ -1,1 +1,4 @@
-30 12 * * * python /reWriter/reWriter.py # Run paraphraser every day at 12:30
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+30 0 * * * cd /reWriter && python ./reWriter.py >> /var/log/cron.log 2>&1 # Run paraphraser at 00:30 every day ( scraper should be done by then )
--- scraper/scrape.cron.sh
+++ scraper/scrape.cron.sh
@@ -1,1 +1,4 @@
-* * * * * /scraper/runspiders.sh >> var/log/cron.log 2>&1 # Run scraper every minute
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+0 0 * * * cd /scraper && ./runspiders.sh >> /var/log/cron.log 2>&1 # Run scraper daily at midnight
+

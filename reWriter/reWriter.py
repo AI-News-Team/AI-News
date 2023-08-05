61c1285bba8011b78e3591828e700ef2e7a2802d
gpseal <sealgp1@student.op.ac.nz>
2023-08-05 22:17:41 +1200

    create function for each reWriter

--- reWriter/reWriter.py
+++ reWriter/reWriter.py
@@ -9,44 +9,17 @@ from requests.exceptions import RequestException
 from transformers import PegasusForConditionalGeneration, PegasusTokenizer
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
-# Pegasus ReWriter-------------------------------------------------
+'''
+Select reWriter model by updating REWRITER_MODEL variable to one of the following:
+    "pegasus" = tuner007/pegasus_paraphrase
+    "t5_base" = google/flan-t5-base
+''' 
 
-# model_name = 'tuner007/pegasus_paraphrase'
-# torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
-# tokenizer = PegasusTokenizer.from_pretrained(model_name)
-# model = PegasusForConditionalGeneration.from_pretrained(model_name).to(torch_device)
+REWRITER_MODEL="t5_base"
 
-# def get_response(input_text,num_return_sequences,num_beams):
-#   batch = tokenizer([input_text],truncation=True,padding='longest',max_length=60, return_tensors="pt").to(torch_device)
-#   translated = model.generate(**batch,max_length=60,num_beams=num_beams, num_return_sequences=num_return_sequences, temperature=1.5)
-#   tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
-#   return tgt_text
-
-# # Altering beams adjusts the number of words the rewriter looks ahead in a sentence that it is paraphrasing.
-# # Higher beams should mean better results, but longer processing time
-# num_beams = 5
-
-# ## Number of paraphrased results returned per sentence
-# num_return_sequences = 1
-
-# --------------------------------------------------------------------
-
-# google/flan-t5-base rewriter -----------------------------------------
-
-tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-base")
-model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-base")
-
-# input_text = 'rewrite the following article: A prolonged heatwave in the US has led to an uptick in severely burned patients who were injured after touching hot surfaces or objects.  Medics in Arizona said some patients were hurt after falling onto asphalt which had been heated by the sun.  The city of Phoenix has now seen a record-breaking 24 consecutive days of temperatures above 110F (43C).  Officials warn that ground temperatures can reach near-boiling, and warn people to seek shelter from the fiery heat.  Dr Kevin Foster of the Arizona Burn Centre tells BBC News that all of the centre\'s 45 hospital beds are currently occupied, and around one-third of those patients have suffered severe contact burns from scorching concrete and asphalt surfaces.  "Summertime is the busy time, so that\'s not surprising, but the numbers are a little bit higher than anticipated," he says, adding that the rate of new patients has outpaced 2022 so far this summer.  Many of the patients are elderly people, who may have fallen after becoming unsteady in the heat, or children who do not get off the ground quickly after falling.  However, he says the "biggest problem" is drug users who are often dehydrated and can faint on sidewalks.  "When people go down to a hot surface and stay there, it only takes 10 to 15 minutes to suffer heat exhaustion, burns and other problems," says Dr Foster, adding that some of the burns are deep, third-degree injuries that will require skin grafts.  The centre is also treating over 150 patients who have not been admitted to hospital, but need to be treated for burns sustained touching hot surfaces, such as metal car seat belts.  The inside of a car or the dark asphalt surface of a road can be far hotter than the air temperature, research has shown. Touching metal or asphalt for only a few seconds can be enough to cause severe burns.'
-# input_ids = tokenizer(input_text, return_tensors="pt").input_ids
-
-# --------------------------------------------------------------------
-
-# outputs = model.generate(input_ids, 
-#                          min_length=10,
-#                          max_length=20,
-#                          length_penalty=2,
-#                          no_repeat_ngram_size=2)
-# print(tokenizer.decode(outputs[0]))
+if REWRITER_MODEL != "pegasus" and REWRITER_MODEL != "t5_base":
+    print("Please select a valid reWriter model")
+    sys.exit("exiting")
 
 dot = os.path.dirname(os.path.realpath(__file__))
 load_dotenv(f"{dot}/../local.env") # todo: refactor this to check for containerized builds, in which case we should use the `virtual.env` file
@@ -55,6 +28,87 @@ API_HOST = os.getenv("API_HOST")
 
 URL = f'http://{API_HOST}:{API_PORT}/article.create'
 getAllURL = f'http://{API_HOST}:{API_PORT}/article.getAll'
+
+# Defining Pegasus ReWriter-------------------------------------------------
+
+model_name = 'tuner007/pegasus_paraphrase'
+torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
+tokenizer = PegasusTokenizer.from_pretrained(model_name)
+model = PegasusForConditionalGeneration.from_pretrained(model_name).to(torch_device)
+
+def get_response(input_text,num_return_sequences,num_beams):
+  batch = tokenizer([input_text],truncation=True,padding='longest',max_length=60, return_tensors="pt").to(torch_device)
+  translated = model.generate(**batch,max_length=60,num_beams=num_beams, num_return_sequences=num_return_sequences, temperature=1.5)
+  tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
+  return tgt_text
+
+# Altering beams adjusts the number of words the rewriter looks ahead in a sentence that it is paraphrasing.
+# Higher beams should mean better results, but longer processing time
+num_beams = 5
+
+## Number of paraphrased results returned per sentence
+num_return_sequences = 1
+
+# --------------------------------------------------------------------
+
+# google/flan-t5-base rewriter -----------------------------------------
+
+tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-base")
+model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-base")
+
+# ------------PEGASUS REWRITER FUNCTION-----------------
+
+def pegasus_reWriter(article):
+    try:
+        print(f"processing {article['id']}")
+        print(f"{len(article['body'])} lines")
+
+        for i in range(len(article['body'])):
+            print(len(article['body']) - i)
+            # removing empty sentences
+            if article['body'][i] == '':
+                del article['body'][i]
+            else:
+                #re-writing article sentence
+                article['body'][i] = get_response(article['body'][i],num_return_sequences,num_beams)[0]
+
+        return article
+    
+    except RequestException as e:
+        print(f"Error rewriting article {article['id']}", e)    
+
+# ------------T5-BASE REWRITER FUNCTION-----------------
+
+def t5_base_reWriter(article):
+    try:
+        print(f"processing {article['id']}")
+        print(f"{len(article['body'])} lines")
+
+        jointArticle = ' '.join(article['body'])
+        input_text = f"rewrite the following text:\n\n{jointArticle}"
+        # get min length of returned article
+        wordCount = len(input_text.split()) 
+        # round down to nearest hundred
+        wordCount = math.floor(wordCount / 100) * 100
+        input_ids = tokenizer(input_text, return_tensors="pt").input_ids
+
+    # --------------------------------------------------------------------
+
+        print(f"processing article {article['id']}......")
+        outputs = model.generate(input_ids, 
+                            min_length=wordCount+100,
+                            max_length=wordCount*2,
+                            length_penalty=2,
+                            num_beams=4,
+                            no_repeat_ngram_size=3)
+        reWrittenArticle = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        article['body'] = reWrittenArticle.split(".")
+        print(article['body'])
+
+        return article
+    
+    except RequestException as e:
+        print(f"Error rewriting article {article['id']}", e)
 
 def send_article(article):
     try: 
@@ -69,6 +123,8 @@ def send_article(article):
     except RequestException as e:
         print('Failed to connect to server: ', e)
 
+
+# fetching articles to re-write
 try: 
     data = requests.get(
         getAllURL
@@ -77,52 +133,22 @@ try:
 except RequestException as e:
     print('Failed to connect to server: ', e)
 
-# data = requests.get('http://{API_HOST}:{API_PORT}/article.getAll').text
-
 print("loading data...")
 articles = json.loads(data)
 
 print("paraphrasing...")
 for article in articles['data']:
 
-    jointArticle = ' '.join(article['body'])
-    print(jointArticle)
-    input_text = f"rewrite the following text:\n\n{jointArticle}"
-    # get min length of returned article
-    wordCount = len(input_text.split()) 
-    # round down to nearest hundred
-    wordCount = math.floor(wordCount / 100) * 100
-    input_ids = tokenizer(input_text, return_tensors="pt").input_ids
-
-# --------------------------------------------------------------------
-
-    print(f"processing article {article['id']}......")
-    outputs = model.generate(input_ids, 
-                         min_length=wordCount+100,
-                         max_length=wordCount*2,
-                         length_penalty=2,
-                         num_beams=4,
-                         no_repeat_ngram_size=3)
-    reWrittenArticle = tokenizer.decode(outputs[0])
-    article['body'] = reWrittenArticle.split(".")
-    print(article['body'])
-
     # formatting body if it isn't a list
-    # if isinstance(article['body'], str):
-    #     article['body'] = article['body'].split(".")
+    if isinstance(article['body'], str):
+        article['body'] = article['body'].split(".")
 
-    # print(f"processing {article['id']}")
-    # print(f"{len(article['body'])} lines")
+    if REWRITER_MODEL == "pegasus":
+        article = pegasus_reWriter(article)
+    elif REWRITER_MODEL == "t5_base":
+        article = t5_base_reWriter(article)
 
-    # for i in range(len(article['body'])):
-    #     # print(len(article['body']) - i)
-    #     # removing empty sentences
-    #     if article['body'][i] == '':
-    #         del article['body'][i]
-    #     else:
-    #         #re-writing article sentence
-    #         # article['body'][i] = get_response(article['body'][i],num_return_sequences,num_beams)[0]
-    #         print("here")
+    print (article)
     print(f"sending {article['id']}")
     send_article(article)
 

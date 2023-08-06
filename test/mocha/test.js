bb6bea34f5392a04c3cbf1f07fe69a8f7ec0c951
glwatkins <georgettewatkins1@gmail.com>
2023-08-07 10:57:42 +1200

    clean up

--- test/mocha/test.js
+++ test/mocha/test.js
@@ -1,30 +1,12 @@
-// //import chai from "chai";
-// //import chaiHttp from "chai-http";
-// //import { describe, it } from "mocha";
-
-// var expect  = require('chai').expect;
-// var request = require('request');
-// const BASE_URL = "localhost:3001";
-
-// it('Main page content', function(done) {
-//     request('http://localhost:3001/article.summary' , function(error, response, body) {
-//       expect(response.status).to.be.equal(200);
-//         done();
-        
-//     });
-//     //expect(body).to.equal('AI Daily');
-// });
-
 import chai from "chai";
 import chaiHttp from "chai-http";
 import { describe, it } from "mocha";
  const BASE_URL = "localhost:3001";
-//const BASE_URL = "http://localhost:3001/article.summary";
 
-chai.use(chaiHttp); // Provides an interface for integration testing
+chai.use(chaiHttp); 
 
-describe("integration - GitHub Gist", () => {
-  it("should get institutions", (done) => {
+describe("api tests", () => {
+  it("staus tests", (done) => {
     chai
       .request(BASE_URL)
       .get("/article.summary")

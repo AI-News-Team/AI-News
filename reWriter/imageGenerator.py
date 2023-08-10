ee3d6721d63b28443fe8b50dad74539d66de24ad
GSeal <sealgp1@student.op.ac.nz>
2023-08-11 10:16:23 +1200

    test dreamlike-photoreal-2.0 image generator

---
+++ reWriter/imageGenerator.py
@@ -0,0 +1,11 @@
+from diffusers import StableDiffusionPipeline
+import torch
+
+model_id = "dreamlike-art/dreamlike-photoreal-2.0"
+pipe = StableDiffusionPipeline.from_pretrained(model_id)
+pipe = pipe.to("cpu")
+
+prompt = "photo, a church in the middle of a field of crops, bright cinematic lighting, gopro, fisheye lens"
+image = pipe(prompt).images[0]
+
+image.save("./result.jpg")

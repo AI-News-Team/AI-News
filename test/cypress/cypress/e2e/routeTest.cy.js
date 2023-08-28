30ff221bd63d2c2525de8e850010ae7d6428d6eb
glwatkins <georgettewatkins1@gmail.com>
2023-08-28 13:53:34 +1200

    loops around and checks the atricals are up will fail ones with no info

--- test/cypress/cypress/e2e/routeTest.cy.js
+++ test/cypress/cypress/e2e/routeTest.cy.js
@@ -11,227 +11,244 @@ describe('showing the basics of site are up', () => {
       cy.title().should('eq', 'AI Daily');
       cy.url().should('include', 'localhost:3000')
         })
-/*
- 
-    it('News sections showing', () => {
-      cy.contains('NEWS').click()
-      cy.url().should('include', '/news')
 
-      let count4 = 2
+    it('News sections showing', () => {
+        cy.contains('NEWS').click()
+      cy.url().should('include', '/news')
+      let start = cy.get('#root').find('a img').then($elements2 =>{ 
+      let clicking = $elements2;
+      let lenele = clicking.length
+      let count4 = lenele
+      if (lenele <= 50){
       for (let i = 0; i < count4; i++) { 
 
-      cy.get('#root').find('a img').then($elements => {
-        const elementsToClick = $elements;
-        count4 = elementsToClick.length;
-        Cypress.config(count4 = elementsToClick.length) 
-        const lenele = elementsToClick.length;
-        cy.log(lenele)
-        cy.log(count4)
-        count4 = lenele
-        cy.wrap(elementsToClick[i]).then(($ele)=> {
-           cy.wrap($ele).click()
-           const isVisible = cy.get("#root").should('be.visible');
-           cy.log(i)
-             cy
-             .wait(1000)
-             .go('back')
-             .wait(1000)}
-             
-        )})};}
-    )
+        cy.get('#root').find('a img').then($elements => {
+          let elementsToClick = $elements;
+          count4 = elementsToClick.length; 
+          lenele = elementsToClick.length;
+          cy.wrap(elementsToClick[i]).each(($ele)=> {
+            cy.wrap($ele).click()
+            const isVisible = cy.get("#root").should('be.visible')
+              cy
+              .wait(50)
+              .go('back')
+              .log("expected: "+ count4)
+              .log("Current: "+ i)
+              .wait(50)})}).then(($btn) => {
+                if (i = count4) {
+                  cy.end()
+                }})}}
+              })
+            })    
 
     
-    it('Sport sections showing', () => {
+    it('Sport route up', () => {
       cy.contains('SPORT').click()
       cy.url().should('include', '/sport')
 
-      let count3 = 2
+      let start = cy.get('#root').find('a img').then($elements2 =>{ 
+      let clicking = $elements2;
+      let lenele = clicking.length
+      let count3 = lenele
+      if (lenele <= 50){
       for (let i = 0; i < count3; i++) { 
       
-      cy.get('#root').find('a img').then($elements => {
-        const elementsToClick = $elements;
-        count3 = elementsToClick.length;
-        Cypress.config(count3 = elementsToClick.length) 
-        const lenele = elementsToClick.length;
-        cy.log(lenele)
-        cy.log(count3)
-        count3= lenele
-        cy.wrap(elementsToClick[i]).then(($ele)=> {
-           cy.wrap($ele).click()
-           const isVisible = cy.get("#root").should('be.visible');
-           cy.log(i)
-             cy
-             .wait(1000)
-             .go('back')
-             .wait(1000)}
-             
-        )})}}
-    )
+        cy.get('#root').find('a img').then($elements => {
+          let elementsToClick = $elements;
+          count6 = elementsToClick.length; 
+          lenele = elementsToClick.length;
+          cy.wrap(elementsToClick[i]).each(($ele)=> {
+            cy.wrap($ele).click()
+            const isVisible = cy.get("#root").should('be.visible')
+              cy
+              .wait(50)
+              .go('back')
+              .log("expected: "+ count3)
+              .log("Current: "+ i)
+              .wait(50)})}).then(($btn) => {
+                if (i = count3) {
+                  cy.end()
+                }})}}})
+                
+              })
     
-    
-  
-    it('GARDENING sections showing', () => {
+    it('GARDENING route up', () => {
       cy.contains('GARDENING').click()
       cy.url().should('include', '/gardening')
-      let count9 = 2
-      for (let i = 0; i < count9; i++) { 
-      cy.get('#root').find('a img').then($elements => {
-        const elementsToClick = $elements;
-        count9 = elementsToClick.length;
-        Cypress.config(count9 = elementsToClick.length) 
-        const lenele = elementsToClick.length;
-        cy.log(lenele)
-        cy.log(count9)
-        cy.wrap(elementsToClick[i]).then(($ele)=> {
-           cy.wrap($ele).click()
-           const isVisible = cy.get("#root").should('be.visible');
-           cy.log(i)
-             cy
-             .wait(1000)
-             .go('back')
-             .wait(1000)})})
-      }}
-    )
 
-     */
-    it('POLITICS sections showing', () => {
+      let start = cy.get('#root').find('a img').then($elements2 =>{ 
+      let clicking = $elements2;
+      let lenele = clicking.length
+      let count9 = lenele
+      if (count9 <= 50){
+      for (let i = 0; i < count9; i++) { 
+        cy.get('#root').find('a img').then($elements => {
+          let elementsToClick = $elements;
+          count9 = elementsToClick.length; 
+          lenele = elementsToClick.length;
+          cy.wrap(elementsToClick[i]).each(($ele)=> {
+            cy.wrap($ele).click()
+            const isVisible = cy.get("#root").should('be.visible')
+              cy
+              .wait(50)
+              .go('back')
+              .log("expected: "+ count9)
+              .log("Current: "+ i)
+              .wait(50)})}).then(($btn) => {
+                if (i = count9) {
+                  cy.end()
+                }})}}else{
+                  count9 = 49
+                }
+              })
+              })
+                
+
+    it('POLITICS route up', () => {
       cy.contains('POLITICS').click()
       cy.url().should('include', '/politics')
 
-      let count6 = cy.get('#root').find('a img').then($elements2 =>{
-      
+      let start = cy.get('#root').find('a img').then($elements2 =>{ 
       let clicking = $elements2;
       let lenele = clicking.length
-      count6 = lenele
-      cy.log(count6.length)
-    
+      let count6 = lenele
+      if (lenele <= 50){
        for (let i = 0; i < count6; i++) { 
       
         cy.get('#root').find('a img').then($elements => {
           let elementsToClick = $elements;
-          count6 = elementsToClick.length;
-          
-          let lenele = elementsToClick.length;
-          count6 = lenele;
-          Cypress.config(count6 = lenele) 
+          count6 = elementsToClick.length; 
+          lenele = elementsToClick.length;
           cy.wrap(elementsToClick[i]).each(($ele)=> {
             cy.wrap($ele).click()
             const isVisible = cy.get("#root").should('be.visible')
-            cy.log(i)
-            cy.log(count6)
               cy
               .wait(50)
               .go('back')
+              .log("expected: "+ count6)
+              .log("Current: "+ i)
               .wait(50)})}).then(($btn) => {
                 if (i = count6) {
                   cy.end()
-                }})}})
-                
-          })
+                 }})}}})
+              })
+        
       
-  
+    it('World route up', () => {
+      cy.contains('WORLD').click()
+      cy.url().should('include', '/world')
 
-//loop the whole test 
-//incrment the route number - localhost:3000/artical/{number here}
-//cy.request('http://localhost:3001/article.getall')
-/*
-  it('articals showing - world', () => {
-    cy.contains('WORLD').click()
-     cy.url().should('include', '/world')
-
-    let count2 = 2
-    for (let i = 0; i < count2; i++) { 
+      let start = cy.get('#root').find('a img').then($elements2 =>{ 
+      let clicking = $elements2;
+      let lenele = clicking.length
+      let count2 = lenele
+      if (lenele >= 50){count2 = 49}
+      for (let i = 0; i < count2; i++) { 
+        cy.get('#root').find('a img').then($elements => {
+          let elementsToClick = $elements;
+          count2 = elementsToClick.length; 
+          lenele = elementsToClick.length;
+          cy.wrap(elementsToClick[i]).each(($ele)=> {
+            cy.wrap($ele).click()
+            const isVisible = cy.get("#root").should('be.visible')
+              cy
+              .wait(50)
+              .go('back')
+              .log("expected: "+ count2)
+              .log("Current: "+ i)
+              .wait(50)})}).then(($btn) => {
+                if (i = count2) {
+                  cy.end()
+                }})}})
+            })
      
-    
-     cy.get('#root').find('a img').then($elements => {
-       const elementsToClick = $elements;
-       count2 = elementsToClick.length;
-       Cypress.config(count2 = elementsToClick.length) 
-       const lenele = elementsToClick.length;
-       cy.log(lenele)
-       cy.log(count2)
-       cy.wrap(elementsToClick[i]).then(($ele)=> {
-          cy.wrap($ele).click()
-          const isVisible = cy.get("#root").should('be.visible');
-          cy.log(i)
-            cy
-            .wait(1000)
-            .go('back')
-            .wait(1000)})})
-     }}
-   )
 
        
-    it('MOTORING sections showing', () => {
-      let count7 = 2
-     for (let i = 0; i < count7; i++) {   
+        it('MOTORING route up', () => {
           cy.contains('MOTORING').click()
           cy.url().should('include', '/motoring')
-        cy.get('#root').find('a img').then($elements => {
-        const elementsToClick = $elements;
-        count7 = elementsToClick.length;
-        Cypress.config(count7 = elementsToClick.length) 
-        const lenele = elementsToClick.length;
-        cy.log(lenele)
-        cy.log(count7)
-        cy.wrap(elementsToClick[i]).then(($ele)=> {
-           cy.wrap($ele).click()
-           const isVisible = cy.get("#root").should('be.visible');
-           cy.log(i)
-             cy
-             .wait(1000)
-             .go('back')
-             .wait(1000)})})
-      }}
-    )
 
-  
+          let start = cy.get('#root').find('a img').then($elements2 =>{ 
+          let clicking = $elements2;
+          let lenele = clicking.length
+          let count7 = lenele
+          if (lenele <= 50){
+          for (let i = 0; i < count7; i++) {   
+            
+              cy.get('#root').find('a img').then($elements => {
+                let elementsToClick = $elements;
+                count6 = elementsToClick.length; 
+                lenele = elementsToClick.length;
+                cy.wrap(elementsToClick[i]).each(($ele)=> {
+                  cy.wrap($ele).click()
+                  const isVisible = cy.get("#root").should('be.visible')
+                    cy
+                    .wait(50)
+                    .go('back')
+                    .log("expected: "+ count7)
+                    .log("Current: "+ i)
+                    .wait(50)})}).then(($btn) => {
+                      if (i = count7) {
+                        cy.end()
+                       }})}}})
+                      })
+
     it('BUSINESS sections showing', () => {
-      let count = 2
-      for (let i = 0; i < count; i++) {  
+
       cy.contains('BUSINESS').click()
       cy.url().should('include', '/business')
-      cy.get('#root').find('a img').then($elements => {
-        const elementsToClick = $elements;
-        count = elementsToClick.length;
-        Cypress.config(count = elementsToClick.length) 
-        const lenele = elementsToClick.length;
-        cy.log(lenele)
-        cy.log(count)
-        cy.wrap(elementsToClick[i]).then(($ele)=> {
-           cy.wrap($ele).click()
-           const isVisible = cy.get("#root").should('be.visible');
-           cy.log(i)
-             cy
-             .wait(1000)
-             .go('back')
-             .wait(1000)})})
-      }}
-    )
 
- 
+      cy.get('#root').find('a img').then($elements2 =>{ 
+      let clicking = $elements2;
+      let lenele = clicking.length
+      let count = lenele
+      if (lenele <= 50){
+      for (let i = 0; i < count; i++) {  
+      cy.get('#root').find('a img').then($elements => {
+        let elementsToClick = $elements;
+        count = elementsToClick.length; 
+        lenele = elementsToClick.length;
+        cy.wrap(elementsToClick[i]).each(($ele)=> {
+          cy.wrap($ele).click()
+          const isVisible = cy.get("#root").should('be.visible')
+            cy
+            .wait(50)
+            .go('back')
+            .log("expected: "+ count)
+            .log("Current: "+ i)
+            .wait(50)})}).then(($btn) => {
+              if (i = count) {
+                cy.end()
+               }})}}})
+              })
+              
     it('CULTURE sections showing', () => {
-      let count8 = 2
-      for (let i = 0; i < count8; i++) { 
       cy.contains('CULTURE').click()
       cy.url().should('include', '/culture')
+
+      cy.get('#root').find('a img').then($elements2 =>{ 
+      let clicking = $elements2;
+      let lenele = clicking.length
+      let count8 = lenele
+      if (lenele <= 50){
+      for (let i = 0; i < count8; i++) { 
+      
       cy.get('#root').find('a img').then($elements => {
-        const elementsToClick = $elements;
-        count8 = elementsToClick.length;
-        Cypress.config(count8 = elementsToClick.length) 
-        const lenele = elementsToClick.length;
-        cy.log(lenele)
-        cy.log(count8)
-        cy.wrap(elementsToClick[i]).then(($ele)=> {
-           cy.wrap($ele).click()
-           const isVisible = cy.get("#root").should('be.visible');
-           cy.log(i)
-             cy
-             .wait(1000)
-             .go('back')
-             .wait(1000)})})
-      }}
-    )*/
-   
-  //});
-    })
+        let elementsToClick = $elements;
+        count8 = elementsToClick.length; 
+        lenele = elementsToClick.length;
+        cy.wrap(elementsToClick[i]).each(($ele)=> {
+          cy.wrap($ele).click()
+          const isVisible = cy.get("#root").should('be.visible')
+            cy
+            .wait(50)
+            .go('back')
+            .log("expected: "+ count8)
+            .log("Current: "+ i)
+            .wait(50)})}).then(($btn) => {
+              if (i = count8) {
+                cy.end()
+               }})}}})
+              }) 
+    
+})

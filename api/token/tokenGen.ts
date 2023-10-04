9c4cd5a0709a1cd16dee66417731872f2069dbf6
ArtemKech <kecha1@student.op.ac.nz>
2023-10-04 15:58:45 +1300

    chore(api/token): generates a token with salts

---
+++ api/token/tokenGen.ts
@@ -0,0 +1,30 @@
+import crypto from 'crypto';
+import bcrypt from 'bcrypt';
+
+// Generates token
+const generateToken = () => {
+  const tokenLength = 32;
+  const buffer = crypto.randomBytes(tokenLength);
+  const token = buffer.toString('hex');
+  return token;
+};
+
+// Generates salt
+const generateSalt = async () => {
+  const saltRounds = 10;
+  return bcrypt.genSalt(saltRounds);
+};
+
+export const generateAppToken = async (component: string, jwtSecret: string, jwtLifetime: string) => {
+  let token = '';
+
+  if (component === 'client' || component === 'scrapy' || component === 'reWriter') {
+    const salt = await generateSalt();
+    const plaintextToken = generateToken();
+    token = await bcrypt.hash(plaintextToken, salt);
+  } else {
+    throw new Error('Invalid component specified');
+  }
+  
+  return token;
+};

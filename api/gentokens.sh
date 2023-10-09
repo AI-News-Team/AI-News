f41ef8e86049285ba0d47ec8ae7cfbe943085537
Artem Kechemaev (1000099996) <KECHA1@student.op.ac.nz>
2023-10-09 14:28:35 +1300

    fix(api/gentokens): fix the issues with SQL queries

--- api/gentokens.sh
+++ api/gentokens.sh
@@ -42,15 +42,13 @@ INSERT INTO Tokens (token, module) VALUES ('$REWRITER_TOKEN', 'reWriter');
 INSERT INTO Permissions (token_id, route_id)
 SELECT
     (SELECT token_id FROM Tokens WHERE token = '$SCRAPY_TOKEN') AS token_id,
-    (SELECT route_id FROM Routes WHERE route_name = '/article.create_raw') AS route_id,
-    '/article.create_raw' AS route;
+    (SELECT route_id FROM Routes WHERE route_name = '/article.create_raw') AS route_id;
 
 -- Insert routes for REWRITER_TOKEN
 INSERT INTO Permissions (token_id, route_id)
 SELECT
     (SELECT token_id FROM Tokens WHERE token = '$REWRITER_TOKEN') AS token_id,
-    (SELECT route_id FROM Routes WHERE route_name = '/article.create') AS route_id,
-    '/article.create' AS route;
+    (SELECT route_id FROM Routes WHERE route_name = '/article.create') AS route_id;
 EOF
 )
 
